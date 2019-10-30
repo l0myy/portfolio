214fb0a5420576e7d88da9adcb91492cb927d038
UKE12364166 <loomyy69@gmail.com>
2019-10-30 21:11:11 +0300

    Changed blocks in the layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -50,26 +50,40 @@
     <div class="row about">
         <div class="col-lg-4 col-md-4 col-sm-12">
             <img  src="{{asset('images/its_me.jpg')}}" class="img-fluid">
-            <span class="text-justify">Engineer - Programmer</span>
         </div>
         <div class="col-lg-8 col-md-8 col-sm-12 desc">
 
             <h3> Anatolii Trofimov </h3>
             <p>
-                ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                I'm an engineer - programmer with more than 3 years of experience.
+            </p>
+            <p>
+                I graduated from the <a href="http://www.mstuca.ru/">University of Civil Aviation</a> (Moscow) with B.A. in Informatics technology.
+                Also, I completed a M.A. in Informatics technology at <a href="https://www.sut.ru/">The Bonch-Bruevich Saint-Petersburg State University of Telecommunications</a>.
+            </p>
+            <p>
+                I have spent the last year developing my skills as an AMS Engineer for <a href="https://www.weigandt-consulting.com/">Weigandt Consulting</a>,
+                where I have improved my skills in SQL, Shell, Git and Soft skills.
+                Also, I took part as a support team member and as a developer team member.
+            </p>
+            <p>
+                In free time I like to play football and learn something new.
+                At this moment I learning PHP framework Laravel and related technologies.
+            </p>
+            <p>
+                So, you can find some pet-projects below :)
             </p>
         </div>
     </div>
 </div>
 
+
+
 <div class="projects" id="projects">
-    <h1 class="text-center">Projects</h1>
+
     <div class="container">
+        <br>
+        <h1 class="text-center">Projects</h1>
         <div class="row">
             <div class="col-lg-5 col-md-16 col-sm-16 text-center">
                 <h3>Address</h3>
@@ -92,18 +106,18 @@
     <div class="container">
         <form>
             <div class="row">
-                <div class="col-lg-4 col-md-4 col-sm-12">
+                <div class="col-lg-6 col-md-6 col-sm-1">
                     <h1>Get in Touch</h1>
                 </div>
-                <div class="col-lg-8 col-md-8 col-sm-12 right">
+                <div class="col-lg-6 col-md-6 col-sm-1 right">
                     <div class="form-group">
-                        <input type="text" class="form-control form-control-lg" placeholder="Your Name" name="">
+                        <input type="text" class="form-control form-control-lg" placeholder="Your Name" name="name">
                     </div>
                     <div class="form-group">
                         <input type="email" class="form-control form-control-lg" placeholder="YourEmail@email.com" name="email">
                     </div>
                     <div class="form-group">
-         <textarea class="form-control form-control-lg">
+         <textarea class="form-control form-control-lg" name="text">
          </textarea>
                     </div>
                     <input type="submit" class="btn btn-secondary btn-block" value="Send" name="">
@@ -118,35 +132,32 @@
 </body>
 
 <!-- Footer -->
-<footer class="page-footer font-small special-color-dark pt-4 justify-content-center">
-
+<footer class="page-footer font-small special-color-dark pt-4">
     <!-- Footer Elements -->
-    <div class="container" >
-
+    <div class="container foot">
         <!-- Social buttons -->
-
         <div class="social github ">
-            <a href="#" target="_blank"><i class="fa fa-github fa-2x"></i></a>
+            <a href="https://github.com/l0myy" target="_blank"><i class="fa fa-github fa-2x"></i></a>
         </div>
-        <div class="social vk" style="margin-top: 15px">
-            <a href="#" target="_blank"><i class="fa fa-vk fa-2x"></i></a>
+        <div class="social vk" >
+            <a href="https://vk.com/id19015615" target="_blank"><i class="fa fa-vk fa-2x"></i></a>
         </div>
         <div class="social telegram">
-            <a href="#" target="_blank"><i class="fa fa-paper-plane fa-2x"></i></a>
+            <a href="https://t.me/l0myy" target="_blank"><i class="fa fa-paper-plane fa-2x"></i></a>
         </div>
         <!-- Social buttons -->
         <br>
-
     </div>
-    <div class="container">
+
+    <div class="container foot">
     <!-- Footer Elements -->
 
     <!-- Copyright -->
-    <div class="footer-copyright text-center py-3">© 2018 Copyright:
-        <a href="https://mdbootstrap.com/education/bootstrap/"> MDBootstrap.com</a>
+    <div class="footer-copyright text-center py-3">© 2019 Copyright: Anatolii Trofimov
     </div>
     <!-- Copyright -->
     </div>
+
 </footer>
 <!-- Footer -->
 
